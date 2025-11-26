7421e628e1dbd615f913e07ce987113d5f8bca83
LUIZ NASH T. NOTARIO <LNTNOTARIO@Complab2.vsuisabel>
2025-11-26 14:06:13 +0800

    Added React components

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer style={{padding: '20px', backgroundColor: '#333', color: 'white', textAlign: 'center'}}>
+            2025 Luiz Notario. All rights reserved
+        </footer>
+    )
+}
+
+export default Footer;
